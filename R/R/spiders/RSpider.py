48572bfa2abcbe4a833e4421df59213b1ab55deb
xxllp <505279206@qq.com>
2015-07-25 12:45:38 +0800

    修改R package的爬虫

--- R/R/spiders/RSpider.py
+++ R/R/spiders/RSpider.py
@@ -1,7 +1,9 @@
 #coding=utf-8
+#####爬取整个CRAN上package的详细信息。其中有些字段需要做些特殊处理，比如import，可以转为字符串，后续做进一步处理，目前的
 from scrapy.contrib.spiders import CrawlSpider, Rule
 from scrapy.selector import HtmlXPathSelector,Selector
 from  R.items import RItem
+from  bs4 import BeautifulSoup
 from scrapy.http import Request
 class test(CrawlSpider):
     name = "R"
@@ -32,6 +34,9 @@ class test(CrawlSpider):
         item['des']=x.xpath("//html/body/p/text()").extract()[0]
         title=x.xpath("//html/body/table/tr/td[1]/text()").extract()
         nei=x.xpath("//html/body/table/tr/td[2]/text()").extract()
+        print title
+        print nei
+        '''
         for i,j in zip(title,nei):
             if i==u'Version:':
                 item['version']=j
@@ -39,18 +44,23 @@ class test(CrawlSpider):
                 item['depends']=j
             elif i==u'Suggests:':
                 item['suggests']=j
-            elif i==u'Pubulished:':
+            elif i==u'Published:':
                 item['published']=j
             elif i==u'Author:':
                 item['author']=j
-            elif i==u'Maintainer':
-                item['Maintainer']=j
-            elif i==u'License':
+            elif i==u'Maintainer:':
+                item['maintainer']=j
+            elif i==u'License:':
                 item['license']=j
-
-
-
-
+        '''
+        item['version']=x.xpath("//html/body/table/tr/td[contains(.,'Version:')]/following-sibling::td/text()").extract()
+        item['imports']= "".join(x.xpath("//html/body/table/tr/td[contains(.,'Imports:')]/following-sibling::td//text()").extract())
+        item['depends']="".join(x.xpath("//html/body/table/tr/td[contains(.,'Depends:')]/following-sibling::td//text()").extract())
+        item['published']=x.xpath("//html/body/table/tr/td[contains(.,'Published:')]/following-sibling::td/text()").extract()
+        item['suggests']="".join(x.xpath("//html/body/table/tr/td[contains(.,'Suggests:')]/following-sibling::td//text()").extract())
+        item['author']=x.xpath("//html/body/table/tr/td[contains(.,'Author:')]/following-sibling::td/text()").extract()
+        item['maintainer']=x.xpath("//html/body/table/tr/td[contains(.,'Maintainer:')]/following-sibling::td/text()").extract()
+        item['license']=x.xpath("//html/body/table/tr/td[contains(.,'License:')]/following-sibling::td/a/text()").extract()
 
         yield item
 
